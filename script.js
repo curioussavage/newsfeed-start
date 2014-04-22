e7932541e6d93bc88a406839b4cbd8863538ef44
Adam Winn <adambwinn@gmail.com>
2014-04-21 21:00:39 -0600

    use variable for array

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
 
+//    function render(article){
+//
+//    };
+
+
 $.ajax('https://api.parse.com/1/classes/NewsItem', 
   {
     beforeSend: function(request) {
-      request.setRequestHeader('X-Parse-Application-Id', 'yFcbP3VHGZigGyqTnRV0m7vlD275M60LFAUbfw3v');
+            request.setRequestHeader('X-Parse-Application-Id', 'yFcbP3VHGZigGyqTnRV0m7vlD275M60LFAUbfw3v');
             request.setRequestHeader('X-Parse-REST-API-Key', 'PV29bK5zFnaICL8lI4Z7aAqzfx358naGok1HWP1N');
         }
     }
 ).done(function(response) {
   //populate list here with jQuery
         console.log(response);
-        
-    for (var i = 0; i < response.results.length; i++) {
+        var articles = response.results;
+    for (var i = 0; i < articles.length; i++) {
 
-        $(".newsfeed").append("<div class="newsitem"><h1>" + response.results[i].title + "</h1> <p> " + response.results[i].body + "</p> </div>");
+        $(".newsfeed").append("<div class='newsitem'><h1>" +
+            articles[i].title + "</h1> <p> " +
+            articles[i].body + "</p> </div>");
+
+      //  render();
 
     }
 
